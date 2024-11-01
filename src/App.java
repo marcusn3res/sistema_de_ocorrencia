c5781c043c88e3dc62a4b062620a4f82338083e5
marcusn3res <marcusyan1303@gmail.com>
2024-10-31 23:57:49 -0300

    feat: criando objeto aluno para enviar ao banco

--- src/App.java
+++ src/App.java
@@ -1,10 +1,20 @@
 import dao.AlunoDao;
 import domain.Aluno;
+import domain.Responsavel;
+import domain.Serie;
+
 import java.text.SimpleDateFormat;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Aluno marcus = new Aluno(1, "Marcus", "12345678901", "1234567", "34912345678","1999-01-01", "M", "email@gmail",1,1);
+
+        Serie serie = new Serie( 1, 2021, "A", null);
+        
+        Responsavel responsavel = new Responsavel(1, "João", "12345678900", "12345678", "Pai", "34911112232","ruan.neres@iftm.edu");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        
+        Aluno marcus = new Aluno(11, "Marcus", "12345678900", "12345678", "34911112232", sdf.parse("1999-01-01"), "M",
+                "Marcus.yan@iftm.edu", serie, responsavel);
         AlunoDao alunoDao = new AlunoDao();
         alunoDao.inserir(marcus);
         System.out.println(marcus);
